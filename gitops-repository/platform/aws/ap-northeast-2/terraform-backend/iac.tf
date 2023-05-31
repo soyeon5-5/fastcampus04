eb0c131c6ed8277fb8d4af83bc9d67993355721a
soyeon5-5 <ems5-5@naver.com>
2023-05-31 15:09:32 +0900

    modify gitops-repository with my settings

--- gitops-repository/platform/aws/ap-northeast-2/terraform-backend/iac.tf
+++ gitops-repository/platform/aws/ap-northeast-2/terraform-backend/iac.tf
@@ -1,3 +1,3 @@
 module "terraform-backend" {
-  source = "git@github.com:soyeon5-5/fastcampus04.git//gitops-repository/platform/aws/ap-northeast-2/terraform-backend?ref=main"
+  source = "https://github.com/soyeon5-5/fastcampus04/tree/109ed9f08cfac3581fd8fa55be7da9be1b21efd1/gitops-repository/platform/aws/ap-northeast-2/terraform-backend?ref=main"
 }

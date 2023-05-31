33a17718e7269d7e68481d40b5223035fbb3b886
soyeon5-5 <ems5-5@naver.com>
2023-05-31 16:27:52 +0900

    main modify to  master

--- platform/aws/ap-northeast-2/terraform-backend/iac.tf
+++ platform/aws/ap-northeast-2/terraform-backend/iac.tf
@@ -1,3 +1,3 @@
 module "terraform-backend" {
-  source = "git@github.com:soyeon5-5/platform-repository.git//aws/ap-northeast-2/terraform-backend?ref=main"
+  source = "git@github.com:soyeon5-5/platform-repository.git//aws/ap-northeast-2/terraform-backend?ref=master"
 }
--- platform/aws/ap-northeast-2/terraform-codes/iac.tf
+++ platform/aws/ap-northeast-2/terraform-codes/iac.tf
@@ -1,3 +1,3 @@
 module "terraform-codes" {
-  source = "git@github.com:soyeon5-5/platform-repository.git//aws/ap-northeast-2/terraform-codes?ref=main"
+  source = "git@github.com:soyeon5-5/platform-repository.git//aws/ap-northeast-2/terraform-codes?ref=master"
 }

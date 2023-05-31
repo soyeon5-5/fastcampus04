44f1169493987bc18d9d8433cfe55cbd2102fd8b
soyeon5-5 <ems5-5@naver.com>
2023-05-31 15:14:04 +0900

    modify gitops-repository with my settings

--- gitops-repository/platform/aws/ap-northeast-2/terraform-codes/iac.tf
+++ gitops-repository/platform/aws/ap-northeast-2/terraform-codes/iac.tf
@@ -1,3 +1,3 @@
 module "terraform-codes" {
-  source = "git@github.com:soyeon5-5/fastcampus04.git//platform-repository/aws/ap-northeast-2/terraform-codes?ref=main"
+  source = "git@github.com:soyeon5-5/fastcampus04.git//platform-repository/aws/ap-northeast-2/terraform-codes?ref=master"
 }
